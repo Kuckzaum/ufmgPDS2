59a7a8e6d07d326eea877a52598c30c69f398b45
Kuckzaum <danihtoledo22@gmail.com>
2020-12-09 19:29:44 -0300

    gitignore

--- VPL3.cpp
+++ VPL3.cpp
@@ -9,16 +9,15 @@ int main() {
      cout<<value2<<" ";
      cout<<&value1<<" ";
      cout<<long_ptr<<" ";
-     // Possível erro
      cout<<(long_ptr== &value1)<<" ";
      unsigned int values[5] = {2,4,6,8,10};
      unsigned int *v_ptr;
      for(int i = 0;i<5;i++) {
           cout<<values[i]<<" ";
      }
-     v_ptr=&values[0];
+     v_ptr=values;
      for(int i=0;i<5;i++){
-          cout<<v_ptr[0+i]<<" ";
+          cout<<*(v_ptr+i)<<" ";
      }
 
      for(int i=0;i<5;i++){
@@ -40,21 +39,3 @@ int main() {
 
      return 0;
 }
-
-/*
-200000 
-200000 
-0x.*? 
-0x.*? 
-1 
-2 4 6 8 10 
-2 4 6 8 10 
-2 4 6 8 10 
-2 4 6 8 10 
-10 10 10 10 
-0x.*? 
-8 
-0x.*? 
-2
-
-*/
