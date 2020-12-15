62bbd15d3c263a40a5c536e41069457eac824670
Kuckzaum <danihtoledo22@gmail.com>
2020-12-15 16:18:59 -0300

    VPL5

--- VPL5.hpp
+++ VPL5.hpp
@@ -12,30 +12,3 @@ struct Estudante {
 };
 
 #endif
-
-/*
-#ifndef LIST_H
-#define LIST_H
-
-#include <iostream>
-using std::cin;
-using std::cout;
-using std::endl;
-
-
-struct Node {
-    int data;
-    Node * next;
-};
-
-struct List {
-    Node * head = nullptr;
-    Node * tail = nullptr;
-
-    void insertNode(int data);
-    void removeNode(int data);
-    void display();
-};
-
-#endif
-*/
